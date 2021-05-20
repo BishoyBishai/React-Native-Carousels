2082a8ff50b42fff4344da595d4810138c626442
bishoybishai <bishoy.bishai@gmail.com>
2021-05-21 01:32:05 +0200

    add colorful carousel

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Colorful } from './src/colorful';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,8 +6,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Colorful />
     </View>
   );
 }
@@ -14,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
 });
---
+++ src/colorful/Colorful.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, Dimensions, Image, StyleSheet, Animated } from "react-native";
+import { data } from "./data";
+import { StatusBar } from "expo-status-bar";
+const { width, height } = Dimensions.get("screen");
+
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+const Colorful = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  return (
+    <View>
+      <StatusBar />
+      <View style={StyleSheet.absoluteFillObject}>
+        {data.map((img, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+          ]; // [previous, current, next]
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1],
+          });
+          return (
+            <Animated.Image
+              key={index}
+              source={{ uri: img }}
+              blurRadius={50}
+              style={[StyleSheet.absoluteFillObject, { opacity }]}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={data}
+        keyExtractor={(_) => _}
+        horizontal
+        pagingEnabled
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                width,
+                height,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                source={{ uri: item }}
+                style={{
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: "cover",
+                  borderRadius: 16,
+                }}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default Colorful;
